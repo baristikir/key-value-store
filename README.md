84a20af5e8eaedb22dc19e5f9391b1786dcf6c42
Baris Tikir <brstkr@icloud.com>
2021-06-25 16:08:49 +0200

    Some stuff

---
+++ README.md
@@ -0,0 +1,15 @@
+# Key Value Store
+
+Key Value Store written in Rust Lang.
+
+## Commands
+
+Type checking
+```bash
+cargo check
+```
+
+Build project, including binary files
+```bash
+cargo build
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::{collections::HashMap};
+
 fn main() {
-    println!("Hello, world!");
+    let mut arguments = std::env::args().skip(1);
+    let key = arguments.next().expect("Key was not found!");
+    let value = arguments.next().unwrap();
+    println!("The key is '{}' and the value is '{}'", key, value);
+    let contents = format!("{}\t{}\n", key, value);
+    std::fs::write("kv.db", contents).unwrap();
+
+    let database = Database::new().expect("Creating Db failed");
+}
+
+struct Database {
+    map: HashMap<String, String>,
+}
+
+impl Database {
+    fn new() -> Result<Database, std::io::Error> {
+        let mut map= HashMap::new();
+        let contents = std::fs::read_to_string("kv.db")?;
+        for line in contents.lines() {
+            let mut pair = line.splitn(2, "\t");
+            let key = pair.next().expect("Expected a key but got nothing!");
+            let value = pair.next().expect("Expected a value but got nothing!");
+
+            map.insert(key.to_owned(), value.to_owned());
+        }
+        Ok(Database {
+            map,
+        })
+    }
 }
